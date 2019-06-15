2a1aa096a7b55d2f668818f865452f5ab4909db6
jstone18 <jason.stone0808@gmail.com>
2019-06-15 17:30:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here
+const var1 = 31;
+const var2 = 2;
+
+const multiply = var1 * var2;
+
+const num1 = Math.ceil(1)
+const num2 = Math.floor(2)
+const random = Math.ceil(Math.random() * (num2 - num1)) + num1;
+
+const mod = 100 % 6;
+
+const max = Math.max(5, 10, 15, 20)
